51d4d0c881939e5d5c0cacf138fc5c7618c4a2d1
KristianPu <kristianpurgar@gmail.com>
2023-05-17 19:37:21 +0200

    feat: Added dotenv

--- index.ts
+++ index.ts
@@ -1,35 +1,38 @@
+import express, { NextFunction, Request, Response } from "express";
 import { logger } from "./middlewares/logger";
-import express from "express";
-const app = express();
-const port = 3000;
+import dotenv from 'dotenv';
+dotenv.config();
 
-app.use((req, res, next) => {
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use((req: Request, res: Response, next: NextFunction) => {
   // Log an info message for each incoming request
   logger.info(`Received a ${req.method} request for ${req.url}`);
   next();
 });
 
 // Handle HTTP GET requests to the root path
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   // Log messages at different log levels
-  logger.log("error", "This is an error message");
-  logger.log("warn", "This is a warning message");
-  logger.log("info", "This is an info message");
-  logger.log("verbose", "This is a verbose message");
-  logger.log("debug", "This is a debug message");
-  logger.log("silly", "This is a silly message");
+  // logger.log("error", "This is an error message");
+  // logger.log("warn", "This is a warning message");
+  // logger.log("info", "This is an info message");
+  // logger.log("verbose", "This is a verbose message");
+  // logger.log("debug", "This is a debug message");
+  // logger.log("silly", "This is a silly message");
 
   // Send a response to the client
   res.send("Hello, world!");
 });
 
 // A route for manually triggering an error
-app.get("/error", (req, res, next) => {
-  throw new Error('This is a test error');
-})
+// app.get("/error", (req: Request, res: Response, next: NextFunction) => {
+//   throw new Error('This is a test error');
+// })
 
 // Handle errors using the logger
-app.use((err, req, res, next) => {
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 
   // Log the error message at the error level
   logger.error(err.message);
--- middlewares/logger.ts
+++ middlewares/logger.ts
@@ -15,4 +15,4 @@ export const logger = winston.createLogger({
       new winston.transports.Console(),
       new winston.transports.File({ filename: "logs/app.log" }),
     ],
-  });
+});
