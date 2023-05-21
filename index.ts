d6a814019b43247f6c520c0eaa6377fb4b008d33
KristianPu <kristianpurgar@gmail.com>
2023-05-21 20:24:45 +0200

    feat: Added security middlewares

--- index.ts
+++ index.ts
@@ -1,11 +1,23 @@
 import express, { NextFunction, Request, Response } from "express";
 import { errorHandler } from "./src/middlewares/errorHandler";
 import { logger } from "./src/middlewares/logger";
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import morgan from 'morgan';
 import dotenv from 'dotenv';
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(helmet());
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use(morgan('combined'));
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   logger.info(`Received a ${req.method} request for ${req.url}`);
--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,7 +1,7 @@
 import { logger } from "./logger";
 import { NextFunction, Request, Response } from "express";
 
-export const errorHandler = (error: Error, request: Request, response: Response, next: NextFunction) {
+export const errorHandler = (error: Error, request: Request, response: Response, next: NextFunction) => {
     logger.error(error);
     response.status(500).send('Internal Server Error');
-  }
+}
